01380cf491c4775b7ae1adf05799e6c1d8d0fda5
EmilMurahas228 <murakhas@list.ru>
2023-03-06 23:14:44 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./App.module.scss";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 
 function App() {
   return (
     <div className={styles.app}>
       <Home />
+      <Footer />
     </div>
   );
 }
